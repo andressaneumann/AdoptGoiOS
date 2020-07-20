667a2d37539813bcb23497c46382bcd3e9761eb1
Andressa <neumm.andressa@gmail.com>
2020-07-20 19:24:04 -0300

    First Commit - Initial Setup

---
+++ AdoptGo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AdoptGo
+//
+//  Created by Andressa Neumann Silva on 20/07/20.
+//  Copyright © 2020 Andressa Neumann Silva. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
