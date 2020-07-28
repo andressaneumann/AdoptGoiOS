f8b6f535b765d628c5a2b4cdc0e21fb09d350022
Andressa <neumm.andressa@gmail.com>
2020-07-28 14:57:12 -0300

    Further adjustments on Detail Page

--- AdoptGo/DetailsPage/Controller/DetailViewController.swift
+++ AdoptGo/DetailsPage/Controller/DetailViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var viewBackground: UIView!
     @IBOutlet weak var petNameLabel: UILabel!
     @IBOutlet weak var petBreedLabel: UILabel!
     @IBOutlet weak var petGenderLabel: UILabel!
     @IBOutlet weak var petImageView: UIImageView!
+    @IBOutlet weak var petDescriptionLabel: UILabel!
     
+    @IBOutlet weak var petSizeLabel: UILabel!
     var petSelected: Pet? = nil
     
     override func viewDidLoad() {
@@ -26,6 +27,8 @@ class DetailViewController: UIViewController {
             self.petBreedLabel.text = pet.breed
             self.petGenderLabel.text = pet.gender.rawValue
             self.petImageView.image = UIImage(named: pet.imagePath)
+            self.petDescriptionLabel.text = pet.description
+            self.petSizeLabel.text = pet.size.rawValue
         }
         
     }
